e2decd75a2670fe0d4bfed52e82ab091c8b4aae9
Crowdbotics <team@crowdbotics.com>
2021-09-17 16:33:33 +0000

    Update models

--- kalisa_app_30642/wsgi.py
+++ kalisa_app_30642/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'kalisa_app_30642.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "kalisa_app_30642.settings")
 
 application = get_wsgi_application()
